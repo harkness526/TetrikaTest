bf44b9a2f2421f432d0f5139ded495365d0681d0
Harkness526 <Harkness5262@gmail.com>
2020-07-03 11:36:05 +0300

    Task1

---
+++ Task1.py
@@ -0,0 +1,29 @@
+def search_pairs(array, k):
+    result = {}
+    for i, a in enumerate(array):
+        for j,b in enumerate(array):
+            if i != j and a + b == k:
+                if b < a:
+                    a,b = b,a 
+                if (a,b) not in result:
+                    result[(a,b)] = True
+                    break
+    return list(result.keys())
+
+array = [1, 2, 6, 5, 3, 4, 7, 8, 3, 2]
+print(search_pairs(array, 5))
+
+def search_pairs(array, k):
+    result = []
+    for i, a in enumerate(array):
+        for j,b in enumerate(array):
+            if i != j and a + b == k:
+                if b < a:
+                    a,b = b,a 
+                if (a,b) not in result:
+                    result.append((a,b))
+                break
+    return result
+
+array = [1, 2, 6, 5, 3, 4, 7, 8, 3, 2]
+print(search_pairs(array, 5))
